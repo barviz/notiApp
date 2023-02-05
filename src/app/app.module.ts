87d3e545d5c9b4edaf2682945925722fd72ca968
barviz <barbaravizgarra@gmail.com>
2023-02-04 21:26:04 -0300

    creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FormComponent } from './components/form/form.component';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FormComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule
