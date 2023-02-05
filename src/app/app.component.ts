181390f5568de92dada4025e217af08e51c7e0d9
barviz <barbaravizgarra@gmail.com>
2023-02-05 20:22:38 -0300

    se muestra data entrante de la api en pantalla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,27 +4,27 @@ import { NoticiasService } from './services/noticias.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-
 export class AppComponent {
   title = 'api-noticias';
+  listadoNoticias: any[] = [];
 
   /* se inyecta el servicio, noticias.service.ts, en el constructor */
-  constructor(private _api: NoticiasService) {}
+  constructor(private _api: NoticiasService) { }
 
   /*
   acá se va a consumir el servicio
   */
   buscarNoticias(parametro: any) {
-
     /*console.log("soy el padre");
     console.log(parametro);*/
 
     //como la fc devuelve un observable se usa el método subscribe
     //con la funcion flecha acceso a la info almacenada
-    this._api.getNoticias(parametro).subscribe(result=>(console.log(result)))
+    this._api.getNoticias(parametro).subscribe((result) => {
+      console.log(result);
+      this.listadoNoticias = result.articles;
+    });
   }
-
 }
-
--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
+
+  /*decorador que recibe la data entrante del
+  componente app.component.ts más especificamente
+  de la variable listadoNoticias (ver app.component.html)
+   */
+  @Input() listNoticias:any;
+
+  constructor(){}
+
+  ngOnInit(): void {
+
+  }
 
 }
