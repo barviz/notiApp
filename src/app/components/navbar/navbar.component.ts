9704cdf85a6580ff0fc37eb947e16853ac626871
barviz <barbaravizgarra@gmail.com>
2023-02-04 22:20:44 -0300

    navbar agregado

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
+  /*
+  decorador que sirve para
+  recibir datos
+  */
+  @Input() titulo: string;
+
+  constructor() {
+    this.titulo = '';
+  }
+
+
+  /*
+  método que se ejecuta cuando se inicializa la página,
+  por única vez carga todo lo que contiene adentro
+  */
+  ngOnInit(): void {
+
+  }
 }
