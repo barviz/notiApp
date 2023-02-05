4aa5804d9cf3d0a816c22e5035e4336be4e10fd5
barviz <barbaravizgarra@gmail.com>
2023-02-04 23:40:55 -0300

    form agregado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FormComponent } from './components/form/form.component';
 import { ListComponent } from './components/list/list.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ListComponent } from './components/list/list.component';
     ListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent {
+
+export class FormComponent implements OnInit {
+
+  categoriaSeleccionada = 'general';
+  paisSeleccionado = 'ar';
+
+  categorias: any[] = [
+    { value: 'general', nombre: 'General' },
+    { value: 'business', nombre: 'Negocios' },
+    { value: 'entertaiment', nombre: 'Entretenimientos' },
+    { value: 'health', nombre: 'Salud' },
+    { value: 'science', nombre: 'Ciencia' },
+    { value: 'sports', nombre: 'Deportes' },
+    { value: 'technology', nombre: 'Tecnología' },
+  ]
+
+  paises: any[] = [
+    { value: 'ar', nombre: 'Argentina' },
+    { value: 'br', nombre: 'Brasil' },
+    { value: 'co', nombre: 'Colombia' },
+    { value: 'jp', nombre: 'Japón' },
+    { value: 'us', nombre: 'Estados Unidos' },
+    { value: 'gb', nombre: 'Reino Unido' },
+    { value: 'rs', nombre: 'Rusia' },
+  ]
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
+
+  buscarNoticia(){
+    console.log(this.categoriaSeleccionada);
+    console.log(this.paisSeleccionado);
+  }
 
 }
